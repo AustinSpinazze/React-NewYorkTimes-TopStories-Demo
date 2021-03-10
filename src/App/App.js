af6c12e5f9255b831471ef7c189ce14f3e47241e
Austin Spinazze <spinazzeaustin@yahoo.com>
2021-03-10 16:18:03 -0600

    Add skeleton version of assignment

--- src/App/App.js
+++ src/App/App.js
@@ -1,44 +1,11 @@
-import React, { Component, Fragment } from 'react';
-import axios from 'axios';
-
-import Home from '../App/Home/Home';
-import Navbar from '../App/common/Navbar/Navbar';
-import './app.css'
-
+import React, { Component } from 'react';
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            topStories: [],
-            loading: true
-        }
-    }
-
     render() {
         return (
-            <Fragment>
-                <Navbar />
-                {this.state.loading ?
-                    <div>Loading...</div> : <Home topStories={this.state.topStories} />
-                }
-            </Fragment>
+            <div>
+                Add Grid of Stories Here
+            </div>
         )
-    }
-
-    componentDidMount() {
-        const url = `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_NY_TIMES_API_KEY}`;
-        try {
-            axios.get(url).then(response => {
-                this.setState((prevState) => ({
-                    ...prevState,
-                    topStories: response.data.results,
-                    loading: false
-                }))
-            })
-        }
-        catch(error) {
-            console.log(error)
-        }
     }
 }
 
